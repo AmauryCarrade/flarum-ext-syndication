ae1726edb6110e5ee51aa2a5cda89dec2f91ac95
Amaury Carrade <amaury@carrade.eu>
2016-12-05 16:23:53 +0100

    Removed live JS update as it doesnt work in Firefox & others.
    The links can be added dynamically using Javascript, but *not removed* (at least in
    Firefox, see this bug: https://bugzilla.mozilla.org/show_bug.cgi?id=380639), so
    the list keeps growing. That's why this is removed. If we find a workaround,
    or this 2007-bug is fixed (one can hope...), we can revert this commit.

--- js/forum/Gulpfile.js
+++
@@ -1,9 +0,0 @@
-var flarum = require('flarum-gulp');
-
-flarum({
-  modules: {
-    'amaurycarrade/flarum-ext-syndication': [
-      'src/**/*.js'
-    ]
-  }
-});
--- src/Listener/AddClientAssetsAndLinks.php
+++ src/Listener/AddClientAssetsAndLinks.php
@@ -10,17 +10,7 @@ class AddClientAssetsAndLinks
 {
     public function subscribe(Dispatcher $events)
     {
-        $events->listen(ConfigureWebApp::class, [$this, 'addAssets']);
         $events->listen(ConfigureWebApp::class, [$this, 'addFeedLinks']);
-    }
-
-    public function addAssets(ConfigureWebApp $event)
-    {
-        if ($event->isForum())
-        {
-            $event->addAssets(__DIR__ . '/../../js/forum/dist/extension.js');
-            $event->addBootstrapper('amaurycarrade/flarum-ext-syndication/main');
-        }
     }
 
     public function addFeedLinks(ConfigureWebApp $event)
